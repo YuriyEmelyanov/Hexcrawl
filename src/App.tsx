210e6d4f73165a2673d414b89e320e02b6c12f20
YuriyEmelyanov <127982412+YuriyEmelyanov@users.noreply.github.com>
2026-06-17 21:03:46 +0300

    Adjust settled region village assignment

--- src/App.tsx
+++ src/App.tsx
@@ -6594,16 +6594,8 @@ function assignSettledLandPoiKinds(options: {
   const hasWater = (poi: AxialHex) => hexTouchesRiverOrLake(poi, rivers, hexTerrainByKey);
 
   if (region.sizeCategory === 'region') {
-    const countVillages = () => Object.values(assigned).filter((kind) => kind === 'village').length;
-
-    assignFirstMatching('village', hasRoadAndWater);
-    assignFirstMatching('village', hasRoadAndWater);
     assignFirstMatching('village', hasRoadAndWater);
     assignFirstMatching('village', hasWater);
-
-    if (countVillages() < 3) {
-      assignFirstMatching('village', hasWater);
-    }
   } else if (region.sizeCategory === 'land' || region.sizeCategory === 'large_region') {
     assignFirstMatching('town', hasRoadAndWater);
     assignFirstMatching('village', hasRoadAndWater);
